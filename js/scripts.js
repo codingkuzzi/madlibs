af9ba4e694a54651c5bd1f4eb0ffb728dba11161
codingkuzzi <annahobby@mail.ru>
2017-06-05 12:05:49 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var name = $("input#name").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".name").text(name);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
     event.preventDefault();
   });
 });
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function() {
+//       var person1Input = $("input#person1").val();
+//       var person2Input = $("input#person2").val();
+//       var animalInput = $("input#animal").val();
+//       var exclamationInput = $("input#exclamation").val();
+//       var verbInput = $("input#verb").val();
+//       var nounInput = $("input#noun").val();
+//     $(".person1").append(person1Input);
+//     $(".person2").append(person2Input);
+//     $(".animal").append(animalInput);
+//     $(".exclamation").append(exclamationInput);
+//     $(".verb").append(verbInput);
+//     $(".noun").append(nounInput);
+
+//     $("#story").show();
+
+//     event.preventDefault();
+//   });
+// });
